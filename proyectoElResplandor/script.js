82e7a0db2304d66436ebcc39779465f0f8a68b4f
ddaniwey <ddaniwey@gmail.com>
2024-12-04 13:46:00 +0100

    Final de la clase, agregando barra

--- proyectoElResplandor/script.js
+++ proyectoElResplandor/script.js
@@ -4,7 +4,7 @@ function ocultarPeliculas(){
     });
 }
 
-function mostrarDrama(){
+
 // DRAMA
 document.querySelector('#boton-drama').addEventListener("click", ()=>{
     // 1. Ocultar películas
@@ -12,10 +12,9 @@ document.querySelector('#boton-drama').addEventListener("click", ()=>{
     // 2. Mostrar las películas de su categoría
     document.querySelectorAll(".drama").forEach((pelicula) => {
         pelicula.style.display='block';
-    });
-});
-};
-function mostrarComedia(){
+    })
+})
+
 // COMEDIA
 document.querySelector('#boton-comedia').addEventListener("click", ()=>{
     // 1. Ocultar películas
@@ -23,11 +22,11 @@ document.querySelector('#boton-comedia').addEventListener("click", ()=>{
     // 2. Mostrar las películas de su categoría
     document.querySelectorAll(".comedia").forEach((pelicula) => {
         pelicula.style.display='block';
-    });
-});
-}
+    })
+})
 
-function mostrarTerror(){
+
+
 // TERROR
 document.querySelector('#boton-terror').addEventListener("click", ()=>{
     // 1. Ocultar películas
@@ -35,6 +34,46 @@ document.querySelector('#boton-terror').addEventListener("click", ()=>{
     // 2. Mostrar las películas de su categoría
     document.querySelectorAll(".terror").forEach((pelicula) => {
         pelicula.style.display='block';
-    });
-});
-};
+    })
+})
+
+// TODAS
+    document.querySelector('#boton-todas').addEventListener("click", () =>{
+        let peliculas = document.querySelectorAll(".pelicula");
+        peliculas.forEach(pelicula => {
+            pelicula.style.display = 'block';
+        });
+    })
+
+// SELECTOR
+document.querySelector('#selectGenero').addEventListener("click", () => {
+    ocultarPeliculas();
+    let selectGenero = document.querySelector('#selectGenero').value;
+        switch(selectGenero) {
+            case 'todas':
+                let todas = document.querySelectorAll(".pelicula");
+                            todas.forEach(pelicula => {
+                                pelicula.style.display = 'block';
+                            });
+                break;
+                case 'drama':
+                    let drama = document.querySelectorAll(".drama");
+                            drama.forEach(pelicula => {
+                                pelicula.style.display = 'block';
+                            });
+                    break;
+                    case 'comedia':
+                        let comedia = document.querySelectorAll(".comedia");
+                            comedia.forEach(pelicula => {
+                                pelicula.style.display = 'block';
+                            });
+                        break;
+                        case 'terror':
+                            let terror = document.querySelectorAll(".terror");
+                            terror.forEach(pelicula => {
+                                pelicula.style.display = 'block';
+                            });
+                            break;
+                
+        }
+})
